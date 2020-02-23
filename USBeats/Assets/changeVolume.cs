a9575c91ee2e9b60d7bc7a8ad4f8bd1905e3d546
alanschnegg <47207920+alanschnegg@users.noreply.github.com>
2020-02-23 04:53:10 +0100

    Volume gestion in menu is now functionnal

---
+++ USBeats/Assets/changeVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeVolume : MonoBehaviour
+{
+    public Slider volume;
+
+    void Start()
+    {
+        volume.value = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = volume.value;
+    }
+}
